19f11d688d8b05a488c7aeab4c42c326bfd81136
Guiblitzkow <165606191+Guiblitzkow@users.noreply.github.com>
2024-05-28 20:05:00 -0300

    Update java.js

--- cep/java/java.js
+++ cep/java/java.js
@@ -1,13 +1,10 @@
-// Lista para armazenar os endereços
 let enderecosSalvos = [];
 
-// Função para adicionar um endereço à lista
 function adicionarEndereco(endereco) {
     enderecosSalvos.push(endereco);
     exibirEnderecosSalvos();
 }
 
-// Função para exibir os endereços salvos
 function exibirEnderecosSalvos() {
     let listaEnderecos = document.getElementById('listaEnderecos');
     listaEnderecos.innerHTML = '';
@@ -43,19 +40,16 @@ function exibirEnderecosSalvos() {
 
         li.appendChild(btnExcluir);
         listaEnderecos.appendChild(li);
-
-        // Adicionar classe para o segundo endereço em diante
+        
         if (indice % 2 !== 0) {
             li.classList.add('direita');
         }
     });
-
-    // Habilitar o botão "Salvar" se houver endereços salvos
+    
     let btnSalvar = document.getElementById('salvar-btn');
     btnSalvar.disabled = enderecosSalvos.length === 0;
 }
 
-// Função para excluir um endereço da lista
 function excluirEndereco(indice) {
     if (indice >= 0 && indice < enderecosSalvos.length) {
         enderecosSalvos.splice(indice, 1);
@@ -63,7 +57,6 @@ function excluirEndereco(indice) {
     }
 }
 
-// Função para salvar um endereço
 function salvarEndereco() {
     // Obter os dados do endereço a partir do resultado exibido
     let cep = document.querySelector('#resultado p:nth-child(1)').textContent.split(': ')[1];
@@ -71,8 +64,7 @@ function salvarEndereco() {
     let bairro = document.querySelector('#resultado p:nth-child(3)').textContent.split(': ')[1];
     let cidade = document.querySelector('#resultado p:nth-child(4)').textContent.split(': ')[1];
     let uf = document.querySelector('#resultado p:nth-child(5)').textContent.split(': ')[1];
-
-    // Criar o objeto de endereço
+    
     let endereco = {
         cep: cep,
         logradouro: logradouro,
@@ -80,14 +72,10 @@ function salvarEndereco() {
         cidade: cidade,
         uf: uf
     };
-
-    // Adicionar o endereço à lista de endereços salvos
+    
     adicionarEndereco(endereco);
 }
 
-
-
-// Função para consultar o endereço
 function consultaEndereco() {
     let cep = document.querySelector('#cep').value;
     if (!cep || cep.length !== 8) {
